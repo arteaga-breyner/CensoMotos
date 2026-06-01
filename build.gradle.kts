ce0851eeea2904642e42acca0f3630cf8ed48614
arteaga-breyner <bvarteagach@gmail.com>
2026-06-01 14:45:22 -0500

    Update Gradle plugins to new versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
-// build.gradle.kts (Project level)
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
 }
